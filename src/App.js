3524c05d40a061dd62a7cc51de81c2f0717c1bb8
NAVYA242 <pendemnavya01@gmail.com>
2022-11-30 16:51:22 +0530

    Props example

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App() {
+  return(
+     <section>
+     <PropsExample name="Deepu" class="CAMS" rollno="11"/>
+     <PropsExample name="Likki" class="biotech" rollno="14"/>
+        {/*<p>this is react</p>
+        <h1>Heading Tag</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZdgV5S9C5GWwq_c7Zz0iuevDw0j4oR6Igzg&usqp=CAU"alt="computer"/>
+        <ClassComponent/>*/}
+      </section>
+  );
+}
+
+export default App;
